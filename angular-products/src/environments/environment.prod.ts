8f7861bd0db1f96358d70747ea12bc6c31d3b318
Edgar <51219984+EdgarTorreshdz@users.noreply.github.com>
2025-09-14 10:41:24 -0500

    Agregando environment

---
+++ angular-products/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // apiUrl: 'http://localhost:3000' // Example API URL for development
+  apiUrl: 'https://products-app-backend-8v3g.onrender.com' // Example API URL for production
+};
--- backend-app/src/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://tu-backend-en-render.onrender.com/' // URL de Render
-};
